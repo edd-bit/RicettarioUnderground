7d4493f11b850d88632bf23a387c8d9d8ee6c3f5
marcodg95 <marco.deigiudici95@gmail.com>
2024-08-12 12:05:12 +0200

    Prima Entity Dipendenti fatta da Marco e creazione della classe Preferiti nel Model

--- RicettarioSpring/src/main/java/com/example/demo/model/Dipendenti.java
+++ RicettarioSpring/src/main/java/com/example/demo/model/Dipendenti.java
@@ -1,5 +1,58 @@
 package com.example.demo.model;
 
-public class Dipendenti {
+import java.io.Serializable;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Dipendenti implements Serializable {
+	private static final long serialVersionUID = 1L;
+ 
+	//id_dipendente(PK),nome, email,passw
+	
+	@Id
+	private int id_dipendente;
+	
+	private String nome;
+	
+	private String email;
+	
+	private String passw;
+
+	public int getId_dipendente() {
+		return id_dipendente;
+	}
+
+	public void setId_dipendente(int id_dipendente) {
+		this.id_dipendente = id_dipendente;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassw() {
+		return passw;
+	}
+
+	public void setPassw(String passw) {
+		this.passw = passw;
+	}
+	
+	
+	
+	
 }
