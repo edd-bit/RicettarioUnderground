7e4b663155c38b3391dc41b0dc59dd7c3ee249b0
Silveralex33 <alexanile04@gmail.com>
2024-08-13 12:44:40 +0200

    repository per ruoli

---
+++ RicettarioSpring/src/main/java/com/example/demo/repository/RuoliRepository.java
@@ -0,0 +1,14 @@
+package com.example.demo.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.demo.model.Ruoli;
+
+public interface RuoliRepository extends JpaRepository<Ruoli, Integer> {
+
+	public List<Ruoli> findByIdRuolo(int id_ruolo);
+
+	public List<Ruoli> findByDescrizione(String descrizione);
+}
