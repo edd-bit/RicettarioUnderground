e13452b6b2aacf70c43156a096ab97fd80ec963a
marcodg95 <marco.deigiudici95@gmail.com>
2024-08-13 12:27:15 +0200

    Fatto DipendentiRepository - Marco

--- RicettarioSpring/src/main/java/com/example/demo/repository/DipendentiRepository.java
+++ RicettarioSpring/src/main/java/com/example/demo/repository/DipendentiRepository.java
@@ -1,5 +1,20 @@
 package com.example.demo.repository;
 
-public interface DipendentiRepository {
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.demo.model.Dipendenti;
+
+public interface DipendentiRepository extends JpaRepository<Dipendenti, Integer> {
+	//controllare perchè Integer
+	
+	// Il findById dovrebbe essere già dato di base, per il momento non lo inseriamo
+	
+	public List<Dipendenti> findByNome(String nome);
+	
+	public List<Dipendenti> findByEmail(String email);
+	
+	public List<Dipendenti> findByNomeAndEmail(String nome, String email);
 
 }
