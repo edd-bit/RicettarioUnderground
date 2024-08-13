f9b1f4cf1b427c68d9becdd53911283df052b1d1
marcodg95 <marco.deigiudici95@gmail.com>
2024-08-13 12:13:27 +0200

    Classe UtentiBuilder rifatta per problemi tecnici - Marco

---
+++ RicettarioSpring/src/main/java/com/example/demo/dto/builder/UtentiBuilder.java
@@ -0,0 +1,28 @@
+package com.example.demo.dto.builder;
+
+import com.example.demo.dto.UtentiDto;
+import com.example.demo.model.Utenti;
+
+public class UtentiBuilder {
+
+	  public static Utenti fromDtoToEntity(UtentiDto utDto) {
+
+	        Utenti ut = new Utenti();
+
+	        ut.setId_utente(utDto.getId_utente());
+	        ut.setEmail_utente(utDto.getEmail_utente());
+	        ut.setPassw_utente(utDto.getPassw_utente());
+	        return ut;
+	    }
+
+	    public static UtentiDto fromEntityToDto(Utenti ut) {
+	        UtentiDto utDto = new UtentiDto();
+
+	        utDto.setId_utente(ut.getId_utente());
+	        utDto.setEmail_utente(ut.getEmail_utente());
+	        utDto.setPassw_utente(ut.getPassw_utente());
+	        return utDto;
+
+	    }
+	
+}
