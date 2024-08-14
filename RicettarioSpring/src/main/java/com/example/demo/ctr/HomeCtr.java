619f29405cf15266d1d340de549e2110304bc058
marcodg95 <marco.deigiudici95@gmail.com>
2024-08-14 11:10:58 +0200

    fatto HomeCtr - Marco

--- RicettarioSpring/src/main/java/com/example/demo/ctr/HomeCtr.java
+++ RicettarioSpring/src/main/java/com/example/demo/ctr/HomeCtr.java
@@ -1,5 +1,15 @@
 package com.example.demo.ctr;
 
-public class HomeCtr {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
+public class HomeCtr {
+//ctr home di RicettarioSpring
+	
+	@GetMapping("/home")
+	public String home() {
+		
+		return "home";
+	}
 }
